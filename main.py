6cb27523c0d6c1a372bf3f74ea218de6828c6af3
Sujay Lokesh <sl5202@nyu.edu>
2020-03-15 10:28:05 -0400

    Implemented approach 1

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sys
+def main(s1,s2):
+    #Approach 1
+    #counting unique values in s1
+    s1 = str(s1).lower()
+    li = []
+    for i in s1:
+        li.append(i)
+    set(li)
+    if len(li) > len(set(li)):
+        print("false")
+    else:
+        print("true")
+    return
 
 if __name__ == "__main__":
-    s1 = 123
-    s2 = 321
-    main()
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+    main(s1,s2)
